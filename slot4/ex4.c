e1bae29d6b041fb2acc706e1dc5f6d9c2d86c39e
anhnhien07 <anhnhien07@gmail.com>
2026-01-16 11:38:12 +0700

    slot4

--- slot4/ex4.c
+++ slot4/ex4.c
@@ -5,10 +5,11 @@ int main(){
 	scanf("%d,%d", &a,&b);
 	char pt;
 	printf("Nhap phep toan : ");
-	scanf("%c\n",&pt);
+	fflush(stdin);
+	scanf("%c",&pt);
 	float kq =(float) a/b;
 	if (b==0)
 	  printf("Khong the chia cho 0");
 	else 
-	  printf("Ket qua : %d/%d = %.2f\n",a,b,kq);
+	  printf("Ket qua : %d%c%d = %.2f\n",a,pt,b,kq);
 }
